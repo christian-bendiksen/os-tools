7c0a45c7370ff069b02c923f6c3da55e30b09ccf
Rune Morling <ermo@aerynos.com>
2025-12-19 12:19:09 +0100

    moss: Tweak system-model preamble slightly
    Signed-off-by: Rune Morling <ermo@aerynos.com>

--- moss/src/cli/mod.rs
+++ moss/src/cli/mod.rs
@@ -34,7 +34,7 @@ mod version;
 /// Generate the CLI command structure
 fn command() -> Command {
     Command::new("moss")
-        .about("Advanced system state manager")
+        .about("Advanced system state & package manager")
         .arg(
             Arg::new("verbose")
                 .short('v')
@@ -263,13 +263,12 @@ fn replace_aliases(args: env::Args) -> Vec<String> {
 
 fn print_system_model_warning(installation: &Installation) {
     eprintln!(
-        "{}: {path:?} is present & therefore active. This means that:
-
- - The system-model is the source of truth and all repositories & packages must be defined from it
- - Any package changes (installs / removes) will be temporary until the system-model is updated
- - The system state can be reverted to match the system-model state with a `moss sync`
- - To disable the system-model, either remove or rename {path:?}
- ",
+        "{}: `{path:?}` is present & therefore active. This means that:
+ 
+- The system-model is the source of truth and defines all repositories & installed packages.
+- Any changes made via `moss` commands will be temporary until the system-model is updated.
+- The system state can be reverted to match the system-model state with a `moss sync`.
+- To disable the system-model, either remove or rename `{path:?}`.",
         "INFO".green(),
         path = installation.system_model_path(),
     );
